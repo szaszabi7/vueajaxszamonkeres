2450ca52c4b5a2bb8f55d9fc724c51ec14c1996c
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-02-08 14:23:53 +0100

    adatok betöltése

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Statues</h1>
+    <table style="margin-left: auto; margin-right: auto;">
+      <thead>
+        <tr>
+          <th>Személy</th>
+          <th>Magasság</th>
+          <th>Ár</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="statue in statues" v-bind:key="statue.id">
+          <td>{{ statue.person }}</td>
+          <td>{{ statue.height }}</td>
+          <td>{{ statue.price }}</td>
+          <td>
+            <button>Törlés</button>
+            <button>Szerkesztés</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      statues: []
+    }
+  },
+  methods: {
+    async loadStatue() {
+      let Response = await fetch('http://127.0.0.1:8000/api/statues')
+      let data = await Response.json()
+      this.statues = data
+    }
+  },
+  mounted() {
+    this.loadStatue()
   }
 }
 </script>
